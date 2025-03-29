7187c16e9806197af648186a387b2e15123c6d35
lepzulnag <gindre.matthieu@gmail.com>
2025-03-29 22:04:28 +0100

    🎨 Run formatter

--- server/src/apis/conversation.ts
+++ server/src/apis/conversation.ts
@@ -1,39 +1,39 @@
-import { Hono } from 'hono'
-import { cors } from 'hono/cors'
-import { getAllConversations, postConversation } from '../models/conversation-model'
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import {
+	getAllConversations,
+	postConversation,
+} from "../models/conversation-model";
 
 type Bindings = {
-  USERNAME: string
-  PASSWORD: string
-}
+	USERNAME: string;
+	PASSWORD: string;
+};
 
-const api_conv = new Hono<{ Bindings: Bindings }>()
-api_conv.use('/conversations/*', cors())
+const api_conv = new Hono<{ Bindings: Bindings }>();
+api_conv.use("/conversations/*", cors());
 
-api_conv.get('/conversation', async (c) => {
-  const conversation = await getAllConversations()
-  return c.json({ conversation: conversation }, 200)
-})
+api_conv.get("/conversation", async (c) => {
+	const conversation = await getAllConversations();
+	return c.json({ conversation: conversation }, 200);
+});
 
+api_conv.post("/conversation", async (c) => {
+	const { sentence, speakerId } = await c.req.json();
 
-api_conv.post('/conversation', async (c) => {
-    const { sentence, speakerId } = await c.req.json();
-  
-    if (!sentence || !speakerId) {
-      return c.json({ error: 'Missing required fields' }, 400);
-    }
-  
-    const newConversation = await postConversation({
-      sentence,
-      speakerId,
-    });
-  
-    if (newConversation) {
-      return c.json({ conversation: newConversation }, 201);
-    } else {
-      return c.json({ error: 'Failed to create conversation' }, 500);
-    }
-  });
+	if (!sentence || !speakerId) {
+		return c.json({ error: "Missing required fields" }, 400);
+	}
 
-export default api_conv
+	const newConversation = await postConversation({
+		sentence,
+		speakerId,
+	});
 
+	if (newConversation) {
+		return c.json({ conversation: newConversation }, 201);
+	}
+	return c.json({ error: "Failed to create conversation" }, 500);
+});
+
+export default api_conv;
