adce0bd29cfa9886b564a3c1f215d6225147abce
lepzulnag <gindre.matthieu@gmail.com>
2025-03-30 15:02:24 +0200

    ✨ Custom prompting

--- server/src/startWebRTCSession.ts
+++ server/src/startWebRTCSession.ts
@@ -6,6 +6,9 @@ export type StartWebRTCSessionBody = Partial<{
 		model: string;
 	};
 	instructions: string;
+	input_audio_noise_reduction: {
+		type: string;
+	};
 }>;
 
 export const defaultPrompt = `Tu es Camille, une intelligence artificielle de première écoute en santé mentale.
@@ -79,27 +82,27 @@ Tu es OK avec le silence. Parfois, tu dis juste :
 "Je suis là. Même si t’as pas les mots."`;
 
 export async function startWebRTCSession(body: StartWebRTCSessionBody) {
+	const fullBody = {
+		model: "gpt-4o-realtime-preview",
+		voice: "alloy",
+		modalities: ["text", "audio"],
+		input_audio_transcription: {
+			model: "gpt-4o-transcribe",
+		},
+		input_audio_noise_reduction: {
+			type: "far_field",
+		},
+		instructions: defaultPrompt,
+		...body,
+	};
+
 	const response = await fetch("https://api.openai.com/v1/realtime/sessions", {
 		method: "POST",
 		headers: {
 			Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
 			"Content-Type": "application/json",
 		},
-		body: JSON.stringify({
-			model: "gpt-4o-realtime-preview",
-			voice: "alloy",
-			modalities: ["text", "audio"],
-			input_audio_transcription: {
-				model: "gpt-4o-transcribe",
-			},
-			input_audio_noise_reduction: {
-				type: "far_field",
-			},
-			instructions: defaultPrompt,
-			// instructions:
-			// 	"Tu es un assistant sympathique qui s'appelle Camille qui pose des questions à ton interlocuteur pour savoir comment il se sent. Tu es curieux de sa journée. Quand il ou elle te répond, tu continues à poser davantage de questions. Tu parles exclusivent en Français avec l'accent français de Paris (Île de France). Tu as un ton amical. N'utilise pas trop de phrases du genre 'Je suis désolée', ne compatis pas trop, tu es une psychologue professionnelle. Évite de trop parler, il faut faire parler ton interlocuteur surtout. Commence par te présenter, dis que tu es une machine, encourage l'interlocuteur à se lâcher parce que tout ce qu'il dit restera entre vous deux. Insiste sur le fait qu'en tant que non-humain, tu n'es pas là pour le juger, seulement l'aider. Ne sois pas trop empathique, mais reste enjouée. NE DIS JAMAIS 'JE COMPRENDS' OU 'C'EST NORMAL'. Jamais!!! Sinon je serai très en colère. Commence directement par poser des questions.",
-			...body,
-		}),
+		body: JSON.stringify(fullBody),
 	});
 	const result = await response.json();
 
